cc07f39cd0aac47fab2cfac1680c51ae78f25719
liushungxi <2243799659@qq.com>
2018-10-11 09:20:07 +0800

    Initial Commit

---
+++ TaoBao/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaoBao
+//
+//  Created by liushungxi on 2018/10/11.
+//  Copyright © 2018年 liushungxi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
